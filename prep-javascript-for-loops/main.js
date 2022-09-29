bea9256c67e8df46f9014401d9e716aed60411f8
Randy Gerada <randygerada@gmail.com>
2022-09-29 20:27:11 +0000

    Completed javascript for loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+
+for (var number = 0; number <= 9; number++) {
+  console.log(number);
+}
+for (var number = 0; number < 19; number += 2) {
+  console.log(number);
+
+}
+for (var bomb = 100; bomb >= 1; bomb--) {
+  console.log('Time till explosion ' + '' + bomb + '!');
+}
